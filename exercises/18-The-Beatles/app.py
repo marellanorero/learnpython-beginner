909cc72625b2666611a9f44059806e994c119dfc
marellanorero <marellanonorero@gmail.com>
2022-07-05 20:15:50 +0000

    revisar errores

--- exercises/18-The-Beatles/app.py
+++ exercises/18-The-Beatles/app.py
@@ -1,1 +1,6 @@
 # Your code here!!
+def sing():
+    text = "let it be, "
+    song = text * 4 + "whisper words of wisdom, " + text *5 + "there will be an answer, let it be" 
+    print(song)
+sing()
---
+++ exercises/19-Bottles-Of-Milk/app.py
@@ -0,0 +1,10 @@
+def number_of_bottles():
+    for i in range(0, 100):
+        if i > 1:
+            print(i + " bottles of milk on the wall, " + i + " bottles of milk. Take one down and pass it around, " + i + " bottles of milk on the wall.")
+        elif i == 1:
+            print(i + " bottle of milk on the wall, " + i + " bottle of milk. Take one down and pass it around, no more bottles of milk on the wall.")
+        else:
+            print("No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, 99 bottles of milk on the wall.")
+            
+number_of_bottles()
